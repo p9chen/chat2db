c51dbfdc9078894a18a84f5d71f7d656f40c92dd
Will <69612639+p9chen@users.noreply.github.com>
2023-10-11 00:11:27 -0400

    try 13B llama 2 model

--- app/chat_chroma.py
+++ app/chat_chroma.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory, ConversationBufferWindowM
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 
+import gradio as gr
 
 # %% Config TODO: move yaml config
 
@@ -17,7 +18,7 @@ INSTRUCTION = "Given the context that has been provided. \n {context}, Answer th
 DEFAULT_SYSTEM_PROMPT = """\
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. """
 
 PERSIST_DIR = "vector_db/db_chroma"
 
@@ -26,7 +27,7 @@ bnb_config = BitsAndBytesConfig(load_in_4bit=True,
     bnb_4bit_compute_dtype=torch.bfloat16,
     bnb_4bit_use_double_quant=False)
 
-model_id = "/home/will/Documents/models/Llama-2-7b-chat-hf"  # "meta-llama/Llama-2-7b-chat-hf"
+model_id = "/home/will/Documents/models/Llama-2-13b-chat-hf"  # "meta-llama/Llama-2-13b-chat-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config = bnb_config, device_map={"": 0})
@@ -48,12 +49,6 @@ def get_prompt(instruction=INSTRUCTION, new_system_prompt=DEFAULT_SYSTEM_PROMPT,
     system_prompt = b_sys + new_system_prompt + e_sys
     prompt_template = b_inst + system_prompt + instruction + e_inst
     return prompt_template
-
-
-# %% Setup DB retriever
-
-
-
 
 
 # %% Create pipeline
@@ -81,9 +76,10 @@ db = Chroma(persist_directory=PERSIST_DIR, embedding_function=embeddings)
 
 # setup retriever
 retriever = db.as_retriever(search_type="mmr",
-                            search_kwargs={'k': 5,
-                                           'fetch_k': 50,
-                                           'lambda_mult': 0.5
+                            search_kwargs={'k': 3,
+                                           'score_threshold': 0.25,
+                                           'fetch_k': 30,
+                                           'lambda_mult': 0.3  # Diversity of results from MMR; 1 for min and 0 for max
                                            }
                             )
 
@@ -104,55 +100,51 @@ class ChatBot:
         self.memory = memory
         self.prompt = prompt
         self.retriever = retriever
+        self.bot = None
 
-    def create_chat_bot(self, max_new_tokens = 512):
+    def create_chat_bot(self, max_new_tokens=512):
         hf_pipe = create_pipeline(max_new_tokens)
         llm = HuggingFacePipeline(pipeline =hf_pipe)
-        qa = ConversationalRetrievalChain.from_llm(
+        self.bot = ConversationalRetrievalChain.from_llm(
             llm=llm,
             retriever=self.retriever,
             memory=self.memory,
             combine_docs_chain_kwargs={"prompt": self.prompt}
         )
-        return qa
+
+    def clear_llm_memory(self):
+        if not self.bot:
+            self.bot.memory.clear()
+
+    def update_prompt(self, sys_prompt):
+        if not self.bot:
+            template = get_prompt(new_system_prompt=sys_prompt)
+
+            prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+
+            self.bot.combine_docs_chain.llm_chain.prompt = prompt
+
+
+# %% POC UI
 
 chat_bot = ChatBot(memory=memory, prompt=prompt)
-
-bot = chat_bot.create_chat_bot()
-
-# %%
-import gradio as gr
-import random
-import time
-
-def clear_llm_memory():
-    bot.memory.clear()
-
-def update_prompt(sys_prompt):
-    template = get_prompt(instruction, sys_prompt)
-
-    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
-
-    bot.combine_docs_chain.llm_chain.prompt = prompt
-
-
-# %% POC
+chat_bot.create_chat_bot()
 
 with gr.Blocks() as demo:
-    update_sys_prompt = gr.Textbox(label = "Update System Prompt")
-    chatbot = gr.Chatbot(label="Chat2DB Bot", height = 300)
+    update_sys_prompt = gr.Textbox(label="Update System Prompt")
+    chatbot = gr.Chatbot(label="Chat2DB Bot", height=300)
     msg = gr.Textbox(label="Question")
     clear = gr.ClearButton([msg, chatbot])
-    clear_memory = gr.Button(value = "Clear LLM Memory")
+    clear_memory = gr.Button(value="Clear LLM Memory")
 
 
     def respond(message, chat_history):
-        bot_message = bot({"question": message})['answer']
+        bot_message = chat_bot.bot({"question": message})['answer']
         chat_history.append((message, bot_message))
         return "", chat_history
 
     msg.submit(respond, inputs=[msg, chatbot], outputs=[msg, chatbot])
-    clear_memory.click(clear_llm_memory)
-    update_sys_prompt.submit(update_prompt, inputs=update_sys_prompt)
+    clear_memory.click(chat_bot.clear_llm_memory)
+    update_sys_prompt.submit(chat_bot.update_prompt, inputs=update_sys_prompt)
 
 demo.launch(share=False, debug=True)
